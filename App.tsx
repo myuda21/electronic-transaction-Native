5eff5f1add4b809b7f6e121f35eb387227450ce1
maxwell yudanegara <maxwellyudanegara52@gmail.com>
2025-06-10 13:52:54 +0700

    feat: fix gradle

--- App.tsx
+++ App.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./src/presentation/navigation/app_navigator";
 import { CartProvider } from "./src/context/cart_context";
+import { Provider as PaperProvider } from "react-native-paper";
 
 export default function App() {
   return (
-    <CartProvider>
-      <NavigationContainer>
-        <AppNavigator />
-      </NavigationContainer>
-    </CartProvider>
+    <PaperProvider>
+      <CartProvider>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </CartProvider>
+    </PaperProvider>
   );
 }
-
