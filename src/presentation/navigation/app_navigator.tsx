67f4eb450490f93474bc7e8eaea0dc4195790281
maxwell yudanegara <maxwellyudanegara52@gmail.com>
2025-06-10 13:52:15 +0700

    feat: hide appbar from the navigation

--- src/presentation/navigation/app_navigator.tsx
+++ src/presentation/navigation/app_navigator.tsx
@@ -11,7 +11,7 @@ const Stack = createStackNavigator();
 export default function AppNavigator() {
     return (
         <Stack.Navigator>
-            <Stack.Screen name= {AppRoute.PRODUCT_LIST} component={ProductListPage} options={{ title: "Produk Elektronik" }} />
+            <Stack.Screen name= {AppRoute.PRODUCT_LIST} component={ProductListPage} options={{ headerShown: false }} />
             <Stack.Screen name={AppRoute.PRODUCT_DETAIL_PAGE} component={ProductDetailPage} />
             <Stack.Screen name={AppRoute.CART_PAGE} component={CartPage} />
             {/* Tambah: CheckoutPage */}
